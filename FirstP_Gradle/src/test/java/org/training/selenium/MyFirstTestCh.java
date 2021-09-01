1f9062249a8fc725727638067946f1d672104a0a
Victoria Karol <v.karol@andersenlab.com>
2021-09-01 12:19:16 +0300

    Create custom timeout functions for isDisplayed and isEnabled.

--- FirstP_Gradle/src/test/java/org/training/selenium/MyFirstTestCh.java
+++ FirstP_Gradle/src/test/java/org/training/selenium/MyFirstTestCh.java
@@ -4,21 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class MyFirstTestCh {
 
     public static void main(String[] args) {
-   
+       
         Singleton singleton = Singleton.getInstance(new ChromeDriver());
         WebDriver driver = singleton.driver;
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
         try {
             driver.get("http://www.bbc.com/");
             driver.findElement(By.id("orb-search-q")).sendKeys("England");
             driver.findElement(By.id("orb-search-button")).click();
             WebElement searchLink = driver.findElement(By.cssSelector("main#main-content li:first-child"));
-            boolean searchLinkPresence = searchLink.isDisplayed();
-            boolean searchLinkEnabled = searchLink.isEnabled();
-            if(searchLinkEnabled==true && searchLinkPresence==true){
+            boolean searchLinkDisplayed = waitForIsDisplayed(searchLink,10);
+            boolean searchLinkEnabled = waitForIsEnabled(searchLink,10);
+            if(searchLinkEnabled && searchLinkDisplayed){
                 searchLink.click();
             }
 
@@ -26,6 +33,36 @@ public class MyFirstTestCh {
             driver.quit();
         }
 
+    }
+
+    public static boolean waitForIsDisplayed(WebElement element,float watingTime){
+        float currentTime=0;
+        float startTime = System.currentTimeMillis();
+        while (!element.isDisplayed()){
+            if(currentTime<=watingTime){
+                currentTime = System.currentTimeMillis()-startTime;
+            }
+            else{
+                System.out.println("!!!TimeOutException in waitForIsDisplayed: condition wasn't executed with time limit");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean waitForIsEnabled(WebElement element, float watingTime){
+        float currentTime=0;
+        float startTime = System.currentTimeMillis();
+        while (!element.isEnabled()){
+            if(currentTime<=watingTime){
+                currentTime = System.currentTimeMillis()-startTime;
+            }
+            else{
+                System.out.println("!!!TimeOutException in waitForIsEnabled: condition wasn't executed with time limit");
+                return false;
+            }
+        }
+        return true;
     }
 
 }
